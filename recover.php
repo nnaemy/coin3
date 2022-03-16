a00a86212388d3feda0dd8f9ec9b8fd0f5ac6516
johnpaul <66500464+nnaemy@users.noreply.github.com>
2022-03-16 10:34:16 -0700

    Update recover.php

--- recover.php
+++ recover.php
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="recover.css">
-</head>
-<body>
-    <?php
-    echo "hello";
-    ?>
-</body>
-</html>
+<?php
+
+try {
+
+    $host = "localhost";
+    $dbname = "coin3";
+    $user = "root";
+    $pass = "";
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, 
+    PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+
+    echo "ok";
+
+}
+
+
+
+?>
